2f1051e5728a1c1ce92f5f88ada746333d3c66bd
Xyphuz <hansen0715.2@gmail.com>
2021-06-12 00:06:19 +0800

    Can copy now.

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
+import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 import 'package:win32/win32.dart';
+import 'package:ffi/ffi.dart';
 
 enum MouseState {
   pressed,
@@ -31,19 +33,47 @@ class _HomePage extends State<HomePage> {
     const Duration captureFrameDuration = Duration(milliseconds: 1);
 
     Timer.periodic(captureFrameDuration, (Timer t) {
-      final MouseState keyState = _isMousePressed();
+      final MouseState mouseState = _isMousePressed();
 
-      if (keyState != _lastMouseState) {
-        debugPrint(keyState.toString());
-        _lastMouseState = keyState;
+      if (mouseState != _lastMouseState) {
+        debugPrint(mouseState.toString());
+        _lastMouseState = mouseState;
+
+        if (mouseState == MouseState.released) {
+          _copyToClipboard();
+        }
       }
     });
   }
 
-  MouseState _isMousePressed() {
-    final int keyState = GetKeyState(QS_KEY);
+  void _copyToClipboard() {
+    // Ctrl+C
 
-    if (keyState == 0 || keyState == 1) {
+    final Pointer<INPUT> kbd = calloc<INPUT>();
+
+    kbd.ref.type = INPUT_KEYBOARD;
+
+    kbd.ki.wVk = 0x11;
+    SendInput(1, kbd, sizeOf<INPUT>());
+
+    kbd.ki.wVk = 0x43;
+    SendInput(1, kbd, sizeOf<INPUT>());
+
+    kbd.ki.wVk = 0x11;
+    kbd.ki.dwFlags = KEYEVENTF_KEYUP;
+    SendInput(1, kbd, sizeOf<INPUT>());
+
+    kbd.ki.wVk = 0x43;
+    kbd.ki.dwFlags = KEYEVENTF_KEYUP;
+    SendInput(1, kbd, sizeOf<INPUT>());
+
+    free(kbd);
+  }
+
+  MouseState _isMousePressed() {
+    final int mouseState = GetKeyState(QS_KEY);
+
+    if (mouseState == 0 || mouseState == 1) {
       return MouseState.released;
     }
 
